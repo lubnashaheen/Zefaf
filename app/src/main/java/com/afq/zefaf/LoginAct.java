cbaf3b07b6a1452f07d5bec44003dcdc8422e675
AfnanQDW <afnan.qdw@gmail.com>
2020-05-07 12:05:19 +0300

    vv

--- app/src/main/java/com/afq/zefaf/LoginAct.java
+++ app/src/main/java/com/afq/zefaf/LoginAct.java
@@ -14,13 +14,13 @@ public class LoginAct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button button=findViewById(R.id.buttonlogin);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent =new Intent(LoginAct.this,Regster.class);
-                startActivity(intent);
-            }
-        });
+//        Button button=findViewById(R.id.buttonlogin);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent =new Intent(LoginAct.this,Regster.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
